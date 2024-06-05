03da0cd4148090b08cfa1635e8d6330034bf52d1
system64fumo <system64fumo@tuta.io>
2024-06-05 03:18:10 +0300

    Cleanup
    Improved the code base
    Added an array to store the network interfaces

--- src/modules/network.hpp
+++ src/modules/network.hpp
@@ -1,10 +1,25 @@
 #include "../module.hpp"
 
+struct network_adapter {
+	std::string interface;
+	std::string type;
+	std::string ipv4;
+};
+
 class module_network : public module {
 	public:
 		module_network(bool icon_on_start = false, bool clickable = false);
 		~module_network();
 
 	private:
-		bool update_info();
+		int nl_socket;
+		char buffer[4096];
+		unsigned int default_if = 0;
+		std::vector<network_adapter> adapters;
+
+		bool setup_netlink();
+		void interface_thread();
+		void update_info();
+		void process_message(struct nlmsghdr *nlh);
+		void request_dump(int nl_socket, int type);
 };
