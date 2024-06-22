963a220f2b02fdd3cfc264f5a84618cb8c19a9a3
system64fumo <system64fumo@protonmail.com>
2024-06-22 23:12:10 +0300

    Optimize
    Made heavier use of constants to avoid copying stuff.
    Removed unnecessary junk.
    
    This is still not enough to please my memory usage desires.

--- src/module.cpp
+++ src/module.cpp
@@ -37,22 +37,24 @@ module::module(const bool &icon_on_start, const bool &clickable) {
 	// TODO: add user customizable margins
 	image_icon.set_size_request(size, size);
 
-	popover_popout.set_child(box_popout);
-	popover_popout.set_has_arrow(false);
-	popover_popout.set_parent(*this);
-
 	// Life would be simpler if buttons would not get stuck in a :hover
 	// or :active state permenantly..
 	if (clickable) {
-		m_click_gesture = Gtk::GestureClick::create();
-		m_click_gesture->set_button(GDK_BUTTON_PRIMARY);
-		m_click_gesture->signal_pressed().connect(sigc::mem_fun(*this, &module::on_clicked));
-		add_controller(m_click_gesture);
+		Glib::RefPtr<Gtk::GestureClick> click_gesture;
+
+		popover_popout.set_child(box_popout);
+		popover_popout.set_has_arrow(false);
+		popover_popout.set_parent(*this);
+
+		click_gesture = Gtk::GestureClick::create();
+		click_gesture->set_button(GDK_BUTTON_PRIMARY);
+		click_gesture->signal_pressed().connect(sigc::mem_fun(*this, &module::on_clicked));
+		add_controller(click_gesture);
 		box_popout.set_size_request(300,400);
 		popover_popout.set_offset(-150,0);
 	}
 }
 
-void module::on_clicked(int n_press, double x, double y) {
+void module::on_clicked(const int &n_press, const double &x, const double &y) {
 	popover_popout.popup();
 }
