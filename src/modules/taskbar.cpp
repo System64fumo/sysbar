48f1ae8c9ded76f0860ceb7106690a20f0d9032b
system64fumo <system64fumo@tuta.io>
2024-08-05 18:44:12 +0300

    Add toplevel labels
    Added toplevel labels to the taskbar entries.

--- src/modules/taskbar.cpp
+++ src/modules/taskbar.cpp
@@ -5,10 +5,55 @@
 #include <gdk/wayland/gdkwayland.h>
 #include <iostream>
 
+// Placeholder functions
+void handle_toplevel_title(void *data, zwlr_foreign_toplevel_handle_v1*, const char *title) {
+	auto toplevel_entry = static_cast<Gtk::Box*>(data);
+	std::string text = title;
+
+	if (text.length() > 10)
+		text = text.substr(0, 10 - 2) + "..";
+
+	Gtk::Label *toplevel_label = Gtk::make_managed<Gtk::Label>(text);
+	toplevel_entry->append(*toplevel_label);
+	std::cout << title << std::endl;
+}
+
+void handle_toplevel_app_id(void *data, zwlr_foreign_toplevel_handle_v1*, const char *app_id) {}
+
+void handle_toplevel_output_enter(void *data, zwlr_foreign_toplevel_handle_v1*, wl_output *output) {}
+
+void handle_toplevel_output_leave(void *data, zwlr_foreign_toplevel_handle_v1*, wl_output *output) {}
+
+void handle_toplevel_state(void *data, zwlr_foreign_toplevel_handle_v1*, wl_array *state) {}
+
+void handle_toplevel_done(void *data, zwlr_foreign_toplevel_handle_v1*) {}
+
+void handle_toplevel_closed(void *data, zwlr_foreign_toplevel_handle_v1* handle) {}
+
+void handle_toplevel_parent(void *data, zwlr_foreign_toplevel_handle_v1* handle, zwlr_foreign_toplevel_handle_v1* parent) {}
+
+zwlr_foreign_toplevel_handle_v1_listener toplevel_handle_v1_impl = {
+	.title = handle_toplevel_title,
+	.app_id = handle_toplevel_app_id,
+	.output_enter = handle_toplevel_output_enter,
+	.output_leave = handle_toplevel_output_leave,
+	.state = handle_toplevel_state,
+	.done = handle_toplevel_done,
+	.closed = handle_toplevel_closed,
+	.parent = handle_toplevel_parent
+};
+
 void handle_manager_toplevel(void *data, zwlr_foreign_toplevel_manager_v1 *manager,
 	zwlr_foreign_toplevel_handle_v1 *toplevel) {
+	auto self = static_cast<module_taskbar*>(data);
 
-	std::cout << "New toplevel appeared" << std::endl;
+	// Temporary placeholder
+	Gtk::Box *toplevel_entry = Gtk::make_managed<Gtk::Box>();
+
+	zwlr_foreign_toplevel_handle_v1_add_listener(toplevel,
+		&toplevel_handle_v1_impl, toplevel_entry);
+
+	self->flowbox_main.append(*toplevel_entry);
 }
 
 zwlr_foreign_toplevel_manager_v1_listener toplevel_manager_v1_impl = {
@@ -34,6 +79,12 @@ wl_registry_listener registry_listener = {
 };
 
 module_taskbar::module_taskbar(const config_bar &cfg, const bool &icon_on_start, const bool &clickable) : module(cfg, icon_on_start, clickable) {
+	image_icon.hide();
+	label_info.hide();
+	if (config_main.position %2 == 0) {
+		flowbox_main.set_orientation(Gtk::Orientation::VERTICAL);
+	}
+	append(flowbox_main);
 	setup_proto();
 }
 
--- src/modules/taskbar.hpp
+++ src/modules/taskbar.hpp
@@ -4,9 +4,13 @@
 #include "../module.hpp"
 #include "../wlr-foreign-toplevel-management-unstable-v1.h"
 
+#include <gtkmm/flowbox.h>
+
 class module_taskbar : public module {
 	public:
 		module_taskbar(const config_bar &cfg, const bool &icon_on_start = false, const bool &clickable = false);
+
+		Gtk::FlowBox flowbox_main;
 
 	private:
 		void setup_proto();
