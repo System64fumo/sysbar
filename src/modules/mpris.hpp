96f33f196405515cdb36beace9d121a59aa9b6a9
system64fumo <system64fumo@tuta.io>
2024-08-30 08:11:19 +0300

    Improve mpris module
    Made the icon change depending on if something is playing or not.

--- src/modules/mpris.hpp
+++ src/modules/mpris.hpp
@@ -3,10 +3,16 @@
 #ifdef MODULE_MPRIS
 
 #include <playerctl.h>
+#include <glibmm/dispatcher.h>
 
 class module_mpris : public module {
 	public:
 		module_mpris(sysbar *window, const bool &icon_on_start = true);
+		int status;
+		Glib::Dispatcher dispatcher_callback;
+
+	private:
+		void update_info();
 };
 
 #endif
