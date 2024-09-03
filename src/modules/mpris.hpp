023ecd787f8deff8751a9b16543a2a1d5399a257
system64fumo <system64fumo@tuta.io>
2024-09-03 22:28:33 +0300

    Add mpris widget
    Added a basic mpris widget, Very dumb looking right now.

--- src/modules/mpris.hpp
+++ src/modules/mpris.hpp
@@ -4,6 +4,7 @@
 
 #include <playerctl.h>
 #include <glibmm/dispatcher.h>
+#include <gtkmm/progressbar.h>
 
 class module_mpris : public module {
 	public:
@@ -15,10 +16,17 @@ class module_mpris : public module {
 		std::string title = "";
 		std::string length = "";
 
+		Gtk::Box box_player;
+		Gtk::Box box_right;
+		Gtk::Label label_title;
+		Gtk::Label label_album;
+		Gtk::Label label_artist;
+		Gtk::ProgressBar progressbar_playback;
 		Glib::Dispatcher dispatcher_callback;
 
 	private:
 		void update_info();
+		void setup_widget();
 };
 
 #endif
