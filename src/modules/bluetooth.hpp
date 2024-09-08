126d9f6927a812d812cfff7c5c47c1c2211473d1
system64fumo <system64fumo@tuta.io>
2024-09-08 18:55:25 +0300

    Add bluetooth devices
    Added a struct for bluetooth devices.
    This should help manage data down the road.
    Also did some cleaning, Put the extract_data() function in the bluetooth module class.

--- src/modules/bluetooth.hpp
+++ src/modules/bluetooth.hpp
@@ -6,10 +6,22 @@
 using DBusPropMap = const Gio::DBus::Proxy::MapChangedProperties&;
 using DBusPropList = const std::vector<Glib::ustring>&;
 
+struct device {
+	std::string path;
+	bool blocked;
+	bool connected;
+	std::string icon;
+	std::string name;
+	bool paired;
+	bool trusted;
+};
+
 class module_bluetooth : public module {
 	public:
 		module_bluetooth(sysbar *window, const bool &icon_on_start = true);
 
 	private:
+		std::vector<device> devices;
 		void update_info(DBusPropMap changed, DBusPropList invalid);
+		void extract_data(const Glib::VariantBase& variant_base);
 };
