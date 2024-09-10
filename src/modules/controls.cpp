bfe6cab1fdab150d045f8b75dae820d7b9f847a7
system64fumo <system64fumo@tuta.io>
2024-09-10 11:04:54 +0300

    Add control class
    Added a control class... What else is there to say?

--- src/modules/controls.cpp
+++ src/modules/controls.cpp
@@ -5,9 +5,30 @@ module_controls::module_controls(sysbar *window, const bool &icon_on_start) : mo
 	image_icon.hide();
 	label_info.hide();
 	append(flowbox_controls);
+	flowbox_controls.set_orientation(Gtk::Orientation::VERTICAL);
+	flowbox_controls.set_homogeneous(true);
+	flowbox_controls.set_hexpand(true);
+	flowbox_controls.set_max_children_per_line(1);
+	flowbox_controls.set_selection_mode(Gtk::SelectionMode::NONE);
 
 	// This causes a critical error.. Why?
 	// TODO: Fix said error
 	auto container = static_cast<Gtk::Box*>(win->box_widgets_end);
 	container->append(*this);
+}
+
+control::control(const bool& extra) {
+	get_style_context()->add_class("control");
+	append(button_action);
+	button_action.get_style_context()->add_class("button_action");
+	button_action.set_has_frame(false);
+	button_action.set_hexpand(true);
+
+	// This is for sub menus
+	if (extra) {
+		append(button_expand);
+		button_action.get_style_context()->add_class("button_expand");
+		button_expand.set_has_frame(false);
+		button_expand.set_icon_name("arrow-right");
+	}
 }
--- src/modules/controls.hpp
+++ src/modules/controls.hpp
@@ -2,6 +2,14 @@
 #include "../module.hpp"
 #ifdef MODULE_CONTROLS
 #include <gtkmm/flowbox.h>
+#include <gtkmm/button.h>
+
+class control : public Gtk::Box {
+	public:
+		control(const bool& extra);
+		Gtk::Button button_action;
+		Gtk::Button button_expand;
+};
 
 class module_controls : public module {
 	public:
