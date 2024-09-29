981aa7ad89e064ac69002b9a03f7935a74770f38
system64fumo <system64fumo@tuta.io>
2024-09-30 00:24:00 +0300

    Improve touch gestures
    Touch gestures are now functional-ish.
    Need to add gestures to the widget container itself too.
    Vertical bar support is still borked.

--- src/window.hpp
+++ src/window.hpp
@@ -4,6 +4,7 @@
 
 #include <gtkmm/revealer.h>
 #include <gtkmm/centerbox.h>
+#include <gtkmm/scrolledwindow.h>
 #include <gtkmm/gesturedrag.h>
 #include <gtkmm/box.h>
 
@@ -27,10 +28,13 @@ class sysbar : public Gtk::Window {
 		Gtk::Box box_center;
 		Gtk::Box box_end;
 
+		Gtk::ScrolledWindow *scrolled_Window_start;
+		Gtk::ScrolledWindow *scrolled_Window_end;
 		Glib::RefPtr<Gtk::GestureDrag> gesture_drag;
 		GdkMonitor *monitor;
 		GdkRectangle monitor_geometry;
 		int width, height;
+		bool sliding_start_widget;
 
 		void load_modules(const std::string &modules, Gtk::Box &box);
 		void setup_controls();
